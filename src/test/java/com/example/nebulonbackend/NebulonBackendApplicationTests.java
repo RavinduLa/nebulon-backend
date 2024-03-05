d57ba116d521879887a1ea6440511a5d22e16230
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2024-03-05 21:22:05 +0530

    Initial commit for Nebulon back-end.

---
+++ src/test/java/com/example/nebulonbackend/NebulonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nebulonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
